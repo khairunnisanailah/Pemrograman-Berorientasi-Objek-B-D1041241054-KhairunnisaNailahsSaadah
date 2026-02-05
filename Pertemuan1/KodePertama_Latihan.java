098d11c5eeb382cce9d8ca886f3b4e764c5ab4f8
khairunnisanailah <d1041241054@student.untan.ac.id>
2026-02-05 14:41:07 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama_Latihan.java
@@ -0,0 +1,9 @@
+
+public class KodePertama_Latihan {
+    public static void main (String[]args){
+        System.out.println("Nama: Khairunnisa Nailah Saadah");
+        System.out.println("NIM: D104124054");
+        System.out.println("Semester: 4");
+        System.out.println("Prodi: Informatika");
+    }
+}
