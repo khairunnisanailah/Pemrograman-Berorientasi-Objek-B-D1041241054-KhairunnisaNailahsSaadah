102b196f1e450c2d5ac364cd282b40e74df644f3
khairunnisanailah <d1041241054@student.untan.ac.id>
2026-02-13 08:48:01 +0700

    p03NamingOperato

---
+++ p03NamingOperator/p041NamingConvention.java
@@ -0,0 +1,20 @@
+package p03NamingOperator;
+/*
+NAMA: KHAIRUNNISA NAILAH SAADAH
+NIM: D1041241054
+TANGGAL: 13/02/2026
+ */
+public class p041NamingConvention {
+public static final double PI=3.14;
+public static void main(String[] args) {
+    //variable dengan camelCase
+    int studentAge = 20;
+    String StudentName = "Budi";
+    boolean isActive = true;
+
+    System.out.println("Nama   : "+StudentName);
+    System.out.println("Umur   : "+studentAge);
+    System.out.println("Status : "+isActive);
+    System.err.println("Konstanta PI: "+PI);
+}
+}
---
+++ p03NamingOperator/p042OperatorAritmatika.java
@@ -0,0 +1,25 @@
+package p03NamingOperator;
+/*
+NAMA: KHAIRUNNISA NAILAH SAADAH
+NIM: D1041241054
+TANGGAL: 13/02/2026
+ */
+public class p042OperatorAritmatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println();
+
+        System.out.println("a + b = "+(a+b));
+        System.out.println("a - b = "+(a-b));
+        System.out.println("a * b = "+(a*b));
+        System.out.println("a / b = "+(a/b));
+        System.out.println("a % b = "+(a%b));
+        
+        //pembagian dengan desimal
+        double hasil = (double) a/b;
+        System.out.println("a / b = "+hasil);
+    }
+}
